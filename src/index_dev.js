1c7e5b31e2930cd9256636b5a56f2930bcf60448
Raimondo Ignizio <raimondoignizio@libero.it>
2021-03-29 10:54:39 +0200

    Various fixes

--- src/index_dev.js
+++ src/index_dev.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import "./css/style.css";
 
-let cityForm = document.getElementById("city-selection");
-let city = cityForm.elements.city.value;
-
-cityForm.onsubmit = async function requestPollutionData() {
-  city = cityForm.elements.city.value;
+async function requestPollutionData() {
   const API_KEY = process.env.API_KEY;
   const result = await axios.get(`https://api.waqi.info/feed/${city}/?token=${API_KEY}`);
   console.log(result);
+}
+
+let getCityInput = function () {
+  event.preventDefault();
+  let city = document.getElementById("city-field").value;
+  console.log(city);
+  requestPollutionData();
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,1 @@
 
-
-
-async function requestPollutionData() {
-  const API_KEY = process.env.API_KEY;
-  const result = await axios.get(`/feed/${city}/?token=${API_KEY}`);
-  console.log(result);
-}
-
-requestPollutionData();
