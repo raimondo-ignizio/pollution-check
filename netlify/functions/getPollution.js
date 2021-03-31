e8e8051cb17271f74ef77ce9fa141c1df9b7c395
Raimondo Ignizio <raimondoignizio@libero.it>
2021-03-31 13:26:59 +0200

    Fixed Netlify function

--- netlify/functions/getPollution.js
+++ netlify/functions/getPollution.js
@@ -1,16 +1,19 @@
 const axios = require("axios");
 
-exports.handler = async event => {
+exports.handler = async function (event, context) {
   const API_KEY = process.env.API_KEY;
-  const city = context.city;
+  const city = event.queryStringParameters.city;
+  console.log(city);
   const response = await axios.get(`https://api.waqi.info/search/?token=${API_KEY}&keyword=${city}`);
+  const cityNameData = response.data.data[0].station.name;
+  const cityAqi = response.data.data[0].aqi;
+  const data = {
+    name: cityNameData,
+    aqi: cityAqi
+  };
 
-  const pass = (body) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(body)
-    }
-  }
-
-  return pass(response);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 import "./css/style.css";
 
+let resultParagraph = document.getElementById("pollution-data");
+
 async function callLambdaFunction(city) {
-  const response = await axios.get("/.netlify/functions/getPollution");
+  const response = await axios.get("/.netlify/functions/getPollution", {
+    params: {
+      city: city
+    }
+  });
   console.log(response);
-  let cityNameData = response.data.data[0].station.name;
-  let cityAqi = response.data.data[0].aqi;
+  let cityNameData = response.data.name;
+  let cityAqi = response.data.aqi;
   resultParagraph.innerHTML = `The Air Quality score in ${cityNameData.bold()} is ${cityAqi.bold()}.`;
 }
 
